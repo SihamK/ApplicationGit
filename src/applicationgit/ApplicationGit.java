95bce4776ea983b876f4e78a5b1b4d07d34ac3ad
SihamK <kadisiham@gmail.com>
2015-01-22 23:36:28 -0300

    Clone a line

--- src/applicationgit/ApplicationGit.java
+++ src/applicationgit/ApplicationGit.java
@@ -17,6 +17,7 @@ public class ApplicationGit {
     public static void main(String[] args) {
         System.out.println("Hello"+ args[0] + "!");
         System.exit(1);
+          System.out.println("Hello"+ args[0] + "!");
     }
     
 }
